995cd613a898d124cc521e270976a835cae5636b
Иван Лещинский <falenangellll@yandex.ru>
2022-06-06 09:53:00 +0700

    remove test component

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export { default as Test } from './test';
-export * from './Table';
 export * from './DataTable';
 export { default as TableWithFixedHeader } from './TableWithFixedHeader/TableWithFixedHeader';
