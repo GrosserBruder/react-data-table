05c488ab905849769e8c319e5a65d42383e3df2d
GrosserBruder <falenangellll@yandex.ru>
2022-06-06 09:34:25 +0700

    Release (#1)

---
+++ src/DataTable/index.ts
@@ -0,0 +1,7 @@
+export { default as DataTable } from "./DataTable";
+export { DataTableProvider } from "./DataTableProvider/DataTableProvider";
+export { useDataTable } from "./DataTableProvider/useDataTable";
+
+export type {
+  BodyLineCell, DataTableProps, LineCell, HeadLineCell, TableRowProps
+} from "./DataTable";
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as Test } from './test';
+export * from './Table';
+export * from './DataTable';
+export { default as TableWithFixedHeader } from './TableWithFixedHeader/TableWithFixedHeader';
