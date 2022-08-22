33de03e0ad14c6993afa6eef3a4c3c1644f8a38b
GrosserBruder <falenangellll@yandex.ru>
2022-08-22 16:11:19 +0700

    remove redundant consts

--- src/const.ts
+++ src/const.ts
@@ -1,26 +1,3 @@
-export enum SORT_VALUES {
-  NOT_SELECTED = 'not_selected',
-  ASC = 'asc',
-  DESC = 'desc'
-}
-
-export const enum VALUE_TYPES {
-  DATE = "date",
-  NUMBER = "number",
-  BOOLEAN = "boolean",
-  STRING = "string",
-}
-
-export type VALUE_TYPE = VALUE_TYPES | null
-
-export const enum FILTER_FIELD_KEY {
-  SEARCH = "search",
-  NUMBER_RANGE = "numberRange",
-  DATE_RANGE = "dateRange",
-  BOOLEAN_FILTER = "boolean_filter",
-  SORT = "sort"
-}
-
 export enum SELECT_STATUSES {
   NOT_SELECTED,
   INDETERMINATE,
