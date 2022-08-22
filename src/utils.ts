b383c84d7fe7c5a2404b7f1c91c0e0f6f34a2b68
GrosserBruder <falenangellll@yandex.ru>
2022-08-22 16:13:06 +0700

    remove redundant utilities

--- src/utils.ts
+++ src/utils.ts
@@ -1,71 +1,10 @@
 import { DataTableColumn } from './DataTable/types';
-import { VALUE_TYPE, VALUE_TYPES } from "./const";
 
 export type ResultCompare = -1 | 0 | 1
-
-export const descSorting = (resultCompare: ResultCompare) => {
-  if (resultCompare === -1) {
-    return 1;
-  }
-  if (resultCompare === 1) {
-    return -1
-  }
-  return 0
-}
-
-export const compareByAlphabetically = (a: string, b: string) => {
-  if (a.toLocaleLowerCase() < b.toLocaleLowerCase()) return -1;
-  if (a.toLocaleLowerCase() > b.toLocaleLowerCase()) return 1;
-  return 0;
-}
-
-export const compareNumberOrBoolean = (a: number | boolean, b: number | boolean) => {
-  if (a < b) return -1;
-  if (a > b) return 1;
-  return 0;
-}
-
-export const compareDate = (a: string | Date, b: string | Date) => {
-  const aDate = new Date(a)
-  const bDate = new Date(b)
-
-  if (aDate < bDate) return -1;
-  if (aDate > bDate) return 1;
-  return 0;
-}
 
 export function primitiveOrFunction(primitiveOrFunction: Function | boolean | string | number | undefined | null, dataToFunction: { [key: string]: any }) {
   if (typeof primitiveOrFunction === "function") return primitiveOrFunction(dataToFunction)
   return primitiveOrFunction;
-}
-
-export const isEmptyDeep = (value: any): boolean => {
-  if (typeof value === "object") {
-    return Object.keys(value).every((x) => isEmptyDeep(x))
-  }
-
-  return !Boolean(value)
-}
-
-export const getValueType = (value: any): VALUE_TYPE => {
-  switch (true) {
-    case (typeof value === "string") && isNaN(+value) && !isNaN(Date.parse(value)):
-      return VALUE_TYPES.DATE
-    case typeof value === "boolean":
-      return VALUE_TYPES.BOOLEAN
-    case typeof value === "number":
-      return VALUE_TYPES.NUMBER
-    case typeof value === "string":
-      return VALUE_TYPES.STRING
-    default:
-      return null
-  }
-}
-
-export const mergeObjects = (first: { [key: string]: any }, second: { [key: string]: any }) => {
-  const firstCopy = { ...first };
-  const secondCopy = { ...second };
-  return Object.assign({}, firstCopy, secondCopy)
 }
 
 export const getColumnKey = (column: DataTableColumn) => {
